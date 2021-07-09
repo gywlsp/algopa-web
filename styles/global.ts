0b4806c51ac22a1e4d5a4207d1cf52ffc58c76ee
gywlsp <gywls517@gmail.com>
2021-07-09 23:13:56 +0900

    chore: :bento: add Comfortaa font

--- styles/global.ts
+++ styles/global.ts
@@ -8,67 +8,85 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   ${normalize}
   * {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  font-family: 'Noto Sans KR', sans-serif;
-}
-
-p {
-  line-height: 1.5;
-}
-
-@media (max-width: 481px) {
-  html {
-    font-size: 8px;
+    box-sizing: border-box;
   }
-}
 
-/* 
-  ##Device = Low Resolution Tablets, Mobiles (Landscape)
-  ##Screen = B/w 481px to 767px
-*/
-
-@media (min-width: 481px) and (max-width: 767px) {
-  html {
-    font-size: 8.5px;
+  body {
+    margin: 0;
+    font-family: 'Noto Sans KR', sans-serif;
   }
-}
 
-/* 
-  ##Device = Tablets, Ipads (landscape)
-  ##Screen = B/w 768px to 1024px
-*/
-
-@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
-  html {
-    font-size: 9px;
+  p {
+    line-height: 1.5;
   }
-}
 
-/* 
-  ##Device = Tablets, Ipads (portrait)
-  ##Screen = B/w 768px to 1024px
-*/
-
-@media (min-width: 768px) and (max-width: 1024px) {
-  html {
-    font-size: 9.5px;
+  @font-face {
+    font-family: 'Comfortaa';
+    font-weight: 500;
+    src: url('/fonts/Comfortaa-Medium.ttf');
   }
-}
 
-/* 
-  ##Device = Laptops, Desktops
-  ##Screen = B/w 1025px to 1280px
-*/
-
-@media (min-width: 1025px) {
-  html {
-    font-size: 10px;
+  @media (max-width: 481px) {
+    html {
+      font-size: 8px;
+    }
   }
-}
+
+  /* 
+    ##Device = Low Resolution Tablets, Mobiles (Landscape)
+    ##Screen = B/w 481px to 767px
+  */
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    html {
+      font-size: 8.5px;
+    }
+  }
+
+  /* 
+    ##Device = Tablets, Ipads (landscape)
+    ##Screen = B/w 768px to 1024px
+  */
+
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+    html {
+      font-size: 9px;
+    }
+  }
+
+  /* 
+    ##Device = Tablets, Ipads (portrait)
+    ##Screen = B/w 768px to 1024px
+  */
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    html {
+      font-size: 9.5px;
+    }
+  }
+
+  /* 
+    ##Device = Laptops, Desktops
+    ##Screen = B/w 1025px to 1280px
+  */
+
+  @media (min-width: 1025px) and (max-width: 1919px) {
+    html {
+      font-size: 10px;
+    }
+  }
+
+  @media (min-width: 1920px) and (max-width: 2559px) {
+    html {
+      font-size: 10.5px;
+    }
+  }
+
+  @media (min-width: 2560px) {
+    html {
+      font-size: 11px;
+    }
+  }
 
 `;
 
