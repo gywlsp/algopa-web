362ed93620e8e2d8311ac3033c1d1a90c6c9d7c5
gywlsp <gywls517@gmail.com>
2021-07-09 23:14:07 +0900

    chore: :label: add IconProps

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+export type IconProps = {
+  style?: React.CSSProperties;
+  fill?: string;
+  fillIn?: string;
+  fillOut?: string;
+  fillDot?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick?: any;
+};
