f5fdf8e1ebb635991dc35c8d8a4b911dbd10ccc7
gywlsp <gywls517@gmail.com>
2021-07-11 20:52:01 +0900

    feat: :sparkles: add GlobalHeader

---
+++ src/layouts/global/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Logo from 'src/components/common/logo';
+import GlobalNav from './nav';
+
+export default function GlobalHeader() {
+  return (
+    <Wrapper>
+      <Logo />
+      <GlobalNav />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  padding: 1.2rem 4rem;
+  box-shadow: 0px 4px 2px -2px rgba(0, 0, 0, 0.2);
+`;
