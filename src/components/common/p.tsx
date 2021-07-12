30902506474408ac8b41d2ebc1cf45517dee54d7
gywlsp <gywls517@gmail.com>
2021-07-12 16:28:07 +0900

    feat: :sparkles: add P

---
+++ src/components/common/p.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { GREY } from 'src/constants/colors';
+
+export type PProps = {
+  level?: number;
+  color?: string;
+  fontWeight?: number;
+  width?: string;
+  height?: string;
+  textAlign?: string;
+  ellipsis?: boolean;
+  preWrap?: boolean;
+  lineHeight?: number | string;
+  numOfLines?: number;
+  className?: string;
+  textDecoration?:
+    | 'none'
+    | 'line-through'
+    | ' overline'
+    | 'underline'
+    | ' initial'
+    | ' inherit';
+} & React.HTMLAttributes<HTMLParagraphElement>;
+
+export default function P(props: PProps) {
+  return <_P {...props} />;
+}
+
+const _P = styled.p<PProps>`
+  ${({
+    width,
+    height,
+    textAlign,
+    ellipsis,
+    preWrap,
+    level,
+    color,
+    fontWeight,
+    lineHeight,
+    numOfLines,
+    textDecoration,
+  }: PProps) => {
+    return `
+      padding: 0;
+      margin: 0;
+      font-size: ${1 + level * 0.2}rem;
+      color: ${color || GREY[800]};
+      font-weight: ${fontWeight};
+      width: ${width || 'fit-content'};
+      height: ${height || 'fit-content'};
+      text-align: ${textAlign || 'left'};
+      text-decoration: ${textDecoration || 'none'};
+      line-height: ${lineHeight || 'normal'};
+      letter-spacing: -0.56px;
+      word-break: break-all;
+      ${
+        ellipsis
+          ? `white-space: nowrap;
+        text-overflow: ellipsis;
+        display: block;
+        overflow: hidden;
+        height:auto;
+      `
+          : ''
+      }
+      ${preWrap ? `white-space: pre-wrap; word-break: keep-all;` : ''}
+      ${
+        numOfLines > 1
+          ? `
+        white-space: normal;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: ${numOfLines};
+        `
+          : ''
+      }
+      `;
+  }}
+`;
