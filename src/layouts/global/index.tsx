d9e4c34ebf545618ae426f3749a0f3bc16d5ac54
gywlsp <gywls517@gmail.com>
2021-07-12 16:29:12 +0900

    feat: :sparkles: add className props to GlobalLayout

--- src/layouts/global/index.tsx
+++ src/layouts/global/index.tsx
@@ -4,21 +4,25 @@ import styled from 'styled-components';
 import GlobalHeader from './header';
 
 export type GlobalLayoutProps = {
+  className?: string;
   children?: ReactNode | ReactNodeArray;
 };
 
-export default function GlobalLayout({ children }: GlobalLayoutProps) {
+export default function GlobalLayout({
+  children,
+  className,
+}: GlobalLayoutProps) {
   return (
     <>
       <GlobalHeader />
-      <ContentWrapper>{children}</ContentWrapper>
+      <ContentWrapper className={className}>{children}</ContentWrapper>
     </>
   );
 }
 
 const ContentWrapper = styled.div`
-  padding: 2rem;
   width: 100%;
   max-width: 128rem;
+  padding: 2rem;
   margin: 0 auto;
 `;
