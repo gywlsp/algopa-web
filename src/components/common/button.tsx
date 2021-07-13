9476bba667c7fdb80f9644195229714ca55bd46c
gywlsp <gywls517@gmail.com>
2021-07-13 16:42:32 +0900

    chore: :lipstick: add Button style changed according to Button type

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -30,6 +30,7 @@ export default function Button({
   if (href) {
     return (
       <StyledLink
+        buttonType={type}
         href={href}
         onClick={onClick}
         hasPadding={_hasPadding}
@@ -42,41 +43,75 @@ export default function Button({
   }
 
   return (
-    <Wrapper hasPadding={_hasPadding} onClick={onClick} className={className}>
+    <Wrapper
+      buttonType={type}
+      hasPadding={_hasPadding}
+      onClick={onClick}
+      className={className}
+    >
       {title}
       {RightIcon}
     </Wrapper>
   );
 }
 
-const Wrapper = styled.button<{ hasPadding: boolean }>`
+const Wrapper = styled.button<{ buttonType: ButtonType; hasPadding: boolean }>`
   display: flex;
   align-items: center;
   margin: 0;
-  padding: ${({ hasPadding }) => (hasPadding ? '0.8rem 1.6rem' : 0)};
+  padding: ${({ hasPadding }) => (hasPadding ? '1rem 1.6rem' : 0)};
   font-size: 1.6rem;
   font-weight: 400;
-  color: ${BLUE_GREEN[500]};
-  background-color: ${WHITE};
-  border: none;
   cursor: pointer;
-  transition: all 0.3s;
+  ${({ buttonType }) =>
+    buttonType === 'primary'
+      ? `color: ${WHITE};
+    background-color: ${BLUE_GREEN[500]};
+    &:hover {
+      background-color: ${BLUE_GREEN[700]}
+    }`
+      : `color: ${BLUE_GREEN[500]};
+    background-color: ${WHITE};
+    &:hover {
+      color: ${BLUE_GREEN[700]}
+    }`};
+  border: ${({ buttonType }) =>
+    buttonType === 'text' ? 'none' : `1px solid ${BLUE_GREEN[500]}`};
+  border-radius: 0.2rem;
   &:hover {
-    color: ${BLUE_GREEN[900]};
+    border-color: ${BLUE_GREEN[700]};
   }
+  transition: all 0.3s;
 `;
 
-const StyledLink = styled(Link)<{ hasPadding: boolean }>`
+const StyledLink = styled(Link)<{
+  buttonType: ButtonType;
+  hasPadding: boolean;
+}>`
   display: flex;
   align-items: center;
   margin: 0;
-  padding: ${({ hasPadding }) => (hasPadding ? '0.8rem 1.6rem' : 0)};
+  padding: ${({ hasPadding }) => (hasPadding ? '1rem 1.6rem' : 0)};
   font-size: 1.6rem;
   font-weight: 400;
-  color: ${BLUE_GREEN[500]};
-  background-color: ${WHITE};
-  transition: all 0.3s;
+  cursor: pointer;
+  ${({ buttonType }) =>
+    buttonType === 'primary'
+      ? `color: ${WHITE};
+  background-color: ${BLUE_GREEN[500]};
   &:hover {
-    color: ${BLUE_GREEN[900]};
+    background-color: ${BLUE_GREEN[700]}
+  }`
+      : `color: ${BLUE_GREEN[500]};
+  background-color: ${WHITE};
+  &:hover {
+    color: ${BLUE_GREEN[700]}
+  }`};
+  border: ${({ buttonType }) =>
+    buttonType === 'text' ? 'none' : `1px solid ${BLUE_GREEN[500]}`};
+  border-radius: 0.2rem;
+  &:hover {
+    border-color: ${BLUE_GREEN[700]};
   }
+  transition: all 0.3s;
 `;
