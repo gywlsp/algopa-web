ef39e65f647256c318e133e3834b88b1290adbcc
gywlsp <gywls517@gmail.com>
2021-07-13 16:45:00 +0900

    feat: :sparkles: add isRouting props to ProblemCard

--- src/components/common/card/problem.tsx
+++ src/components/common/card/problem.tsx
@@ -8,6 +8,7 @@ export type ProblemCardProps = {
   id?: number | string;
   title?: string;
   level?: string;
+  isRouting?: boolean;
   className?: string;
 };
 
@@ -15,10 +16,21 @@ export default function ProblemCard({
   className,
   id = 1260,
   level,
+  isRouting = true,
   title = '문제 제목',
 }: ProblemCardProps) {
+  if (isRouting) {
+    return (
+      <StyledLink href={`/problems/${id}`} className={className}>
+        <Level>난이도</Level>
+        <P>{id}</P>
+        <P>{title}</P>
+      </StyledLink>
+    );
+  }
+
   return (
-    <Wrapper href={`/problems/${id}`} className={className}>
+    <Wrapper className={className}>
       <Level>난이도</Level>
       <P>{id}</P>
       <P>{title}</P>
@@ -26,7 +38,16 @@ export default function ProblemCard({
   );
 }
 
-const Wrapper = styled(Link)`
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin-right: 1.2rem;
+  padding: 2rem;
+  border: 1px solid ${BLUE_GREEN[500]};
+`;
+
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
