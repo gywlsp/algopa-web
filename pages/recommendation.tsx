b055e3f9084720852c8472320d1b6074a709c643
gywlsp <gywls517@gmail.com>
2021-07-15 21:31:29 +0900

    refactor: :recycle: replace prop name isSub with size on Section

--- pages/recommendation.tsx
+++ pages/recommendation.tsx
@@ -10,21 +10,21 @@ export default function RecommendationPage() {
   return (
     <GlobalLayout>
       <Section title="박효진님을 위한 추천 문제">
-        <Section isSub title="다음으로 풀면 좋은 문제들이에요 😉">
+        <Section size="medium" title="다음으로 풀면 좋은 문제들이에요 😉">
           <HorizontalScrollable>
             {[...Array(8)].map((_, i) => (
               <StyledProblemCard key={i} index={i + 1} />
             ))}
           </HorizontalScrollable>
         </Section>
-        <Section isSub title="이런 유형의 문제를 많이 틀려요 😢">
+        <Section size="medium" title="이런 유형의 문제를 많이 틀려요 😢">
           <HorizontalScrollable>
             {[...Array(8)].map((_, i) => (
               <StyledProblemCard key={i} index={i + 1} />
             ))}
           </HorizontalScrollable>
         </Section>
-        <Section isSub title="이런 유형의 문제를 많이 풀지 않았어요 🧐">
+        <Section size="medium" title="이런 유형의 문제를 많이 풀지 않았어요 🧐">
           <HorizontalScrollable>
             {[...Array(8)].map((_, i) => (
               <StyledProblemCard key={i} index={i + 1} />
--- src/components/common/section.tsx
+++ src/components/common/section.tsx
@@ -2,23 +2,39 @@ import React, { ReactNode, ReactNodeArray } from 'react';
 import styled from 'styled-components';
 
 import { GREY } from 'src/constants/colors';
+import P from './p';
+
+export type SectionSize = 'large' | 'medium' | 'small';
 
 export type SectionProps = {
   title: string;
-  isSub?: boolean;
+  size?: SectionSize;
   children: ReactNode | ReactNodeArray;
   className?: string;
 };
 
+const TitleLevel = {
+  large: 7,
+  medium: 5,
+  small: 4,
+};
+
 export default function Section({
   title,
-  isSub = false,
+  size = 'large',
   children,
   className,
 }: SectionProps) {
   return (
     <Wrapper className={className}>
-      <Title isSub={isSub}>{title}</Title>
+      <Title
+        sectionSize={size}
+        level={TitleLevel[size]}
+        fontWeight={500}
+        color={GREY[800]}
+      >
+        {title}
+      </Title>
       {children}
     </Wrapper>
   );
@@ -29,11 +45,11 @@ const Wrapper = styled.div`
   margin: 2rem 0;
 `;
 
-const Title = styled.p<{ isSub: boolean }>`
-  ${({ isSub }) =>
-    isSub
-      ? 'font-size: 2rem; margin-bottom: 1.2rem;'
-      : 'font-size: 2.4rem; margin-bottom: 2rem;'}
-  font-weight: 500;
-  color: ${GREY[800]};
+const Title = styled(P)<{ sectionSize: SectionSize }>`
+  margin-bottom: ${({ sectionSize }) =>
+    sectionSize === 'large'
+      ? '2rem'
+      : sectionSize === 'medium'
+      ? '1.2rem'
+      : '0,8rem'};
 `;
