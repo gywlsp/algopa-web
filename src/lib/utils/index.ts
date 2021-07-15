8fc817019970dbfe8b0955076f8d187bed100c3e
gywlsp <gywls517@gmail.com>
2021-07-15 21:51:04 +0900

    feat: :sparkles: add validateNicknameFormat util

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -3,3 +3,8 @@ export const getQueryString = (queryParameter: Record<string, any>) =>
     const [key, value] = curr;
     return acc.concat(`${i ? '&' : '?'}${key}=${value}`);
   }, '');
+
+export const validateNicknameFormat = (nickname: string): boolean => {
+  const regex = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
+  return regex.test(nickname);
+};
