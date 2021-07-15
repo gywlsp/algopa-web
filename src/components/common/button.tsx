17a78afef8f4d5bb2a6abffff49a0868dda68caa
gywlsp <gywls517@gmail.com>
2021-07-15 21:40:14 +0900

    feat: :sparkles: add Button props
    size, block

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,25 +4,30 @@ import styled from 'styled-components';
 import Link from './link';
 import { BLUE_GREEN, WHITE } from 'src/constants/colors';
 
-type ButtonType = 'primary' | 'secondary' | 'text';
+export type ButtonSize = 'large' | 'medium';
+export type ButtonType = 'primary' | 'secondary' | 'text';
 
 export type ButtonProps = {
+  size?: ButtonSize;
   type?: ButtonType;
   title: string;
   href?: string;
   onClick?: (e?: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   RightIcon?: ReactNode | ReactNodeArray;
   className?: string;
+  block?: boolean;
   hasPadding?: boolean;
 };
 
 export default function Button({
+  size = 'medium',
   type = 'text',
   title,
   href,
   onClick,
   RightIcon,
   className,
+  block = false,
   hasPadding,
 }: ButtonProps) {
   const _hasPadding = hasPadding || type !== 'text';
@@ -30,11 +35,13 @@ export default function Button({
   if (href) {
     return (
       <StyledLink
+        size={size}
         buttonType={type}
         href={href}
         onClick={onClick}
-        hasPadding={_hasPadding}
         className={className}
+        block={block}
+        hasPadding={_hasPadding}
       >
         {title}
         {RightIcon}
@@ -44,10 +51,12 @@ export default function Button({
 
   return (
     <Wrapper
+      size={size}
       buttonType={type}
-      hasPadding={_hasPadding}
       onClick={onClick}
       className={className}
+      block={block}
+      hasPadding={_hasPadding}
     >
       {title}
       {RightIcon}
@@ -55,13 +64,22 @@ export default function Button({
   );
 }
 
-const Wrapper = styled.button<{ buttonType: ButtonType; hasPadding: boolean }>`
+const Wrapper = styled.button<{
+  size: ButtonSize;
+  buttonType: ButtonType;
+  block: boolean;
+  hasPadding: boolean;
+}>`
   display: flex;
+  justify-content: center;
   align-items: center;
+  width: ${({ block }) => (block ? '100%' : 'fit-content')};
+  ${({ size }) =>
+    size === 'medium'
+      ? 'height: 4rem; font-size: 1.6rem; font-weight: 400;'
+      : 'height: 5.2rem; font-size: 1.8rem; font-weight: 500;'}
   margin: 0;
   padding: ${({ hasPadding }) => (hasPadding ? '1rem 1.6rem' : 0)};
-  font-size: 1.6rem;
-  font-weight: 400;
   cursor: pointer;
   ${({ buttonType }) =>
     buttonType === 'primary'
@@ -85,15 +103,22 @@ const Wrapper = styled.button<{ buttonType: ButtonType; hasPadding: boolean }>`
 `;
 
 const StyledLink = styled(Link)<{
+  size: ButtonSize;
   buttonType: ButtonType;
+  block: boolean;
   hasPadding: boolean;
 }>`
   display: flex;
+  justify-content: center;
   align-items: center;
+  width: ${({ block }) => (block ? '100%' : 'fit-content')};
+  ${({ size }) =>
+    size === 'medium'
+      ? 'height: 4rem; font-size: 1.6rem; font-weight: 400;'
+      : 'height: 5.2rem; font-size: 1.8rem; font-weight: 500;'}
   margin: 0;
   padding: ${({ hasPadding }) => (hasPadding ? '1rem 1.6rem' : 0)};
   font-size: 1.6rem;
-  font-weight: 400;
   cursor: pointer;
   ${({ buttonType }) =>
     buttonType === 'primary'
