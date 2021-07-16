e855090db06bdc1ff67946c82a80e3432aa9399c
gywlsp <gywls517@gmail.com>
2021-07-16 11:44:58 +0900

    feat: :sparkles: add LoginRedirectPage logic

--- pages/login-redirect.tsx
+++ pages/login-redirect.tsx
@@ -1,14 +1,44 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/dist/client/router';
 
+import { getEncryptedString } from 'src/lib/utils/crypto';
+import AuthService from 'src/services/api/auth';
+
 export default function LoginRedirectPage() {
   const router = useRouter();
+
   useEffect(() => {
-    const { code } = router.query;
-    if (router.query.code) {
-      console.log(code);
-      router.push('/');
-    }
+    const login = async () => {
+      if (!(router?.query?.code && router?.query?.scope)) {
+        return;
+      }
+      const { code, scope } = router.query;
+      const provider = scope.includes('google') ? 'google' : 'github';
+      try {
+        await AuthService.login(provider, code as string);
+        alert('로그인 성공!');
+        router.push('/');
+      } catch (err) {
+        switch (err.response.status) {
+          // case 401:
+          //   @401 메세지 필요시 추가
+          case 404:
+            const { accessToken, email } = err?.response?.data;
+            const encryptedAccessToken = getEncryptedString(accessToken);
+            alert('가입되지 않은 계정입니다. 회원가입 페이지로 이동합니다.');
+            router.push(
+              `/join?email=${email}&pv=${provider}&at=${encryptedAccessToken}`
+            );
+            break;
+          default:
+            alert('로그인에 실패했습니다.');
+            router.push('/login');
+            break;
+        }
+      }
+    };
+
+    login();
   }, [router.query]);
 
   return <></>;
