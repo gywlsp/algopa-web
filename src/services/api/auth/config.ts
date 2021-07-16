6b46b2788a0f5506c1b9b341e8a63042ac6453fb
gywlsp <gywls517@gmail.com>
2021-07-16 17:06:37 +0900

    feat: :sparkles: add Auth services
    login, validateNickname, getBojIdAuthToken, authenticateBojId, join

---
+++ src/services/api/auth/config.ts
@@ -0,0 +1,20 @@
+import { IUser } from 'src/interfaces/user/IUser';
+import { Provider } from 'src/types/user';
+import { baseConfig } from '..';
+
+export const loginConfig = (provider: Provider, token: string) =>
+  baseConfig(true, token).get(`/auth/v1/login/${provider}`);
+
+export const validateNicknameConfig = (nickname: string) =>
+  baseConfig().get(`/auth/v1/validation/${nickname}`);
+
+export const getBojAuthTokenConfig = (bojId: string) =>
+  baseConfig().get(`/auth/v1/authentication/boj/token/${bojId}`);
+
+export const authenticateBojIdConfig = (bojId: string) =>
+  baseConfig().get(`/auth/v1/authentication/boj/${bojId}`);
+
+export const joinConfig = (
+  data: Pick<IUser, 'email' | 'nickname' | 'provider' | 'bojId'>,
+  token: string
+) => baseConfig(true, token).post(`/auth/v1/join/${data.provider}`, data);
---
+++ src/services/api/auth/index.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+import {
+  loginConfig,
+  authenticateBojIdConfig,
+  validateNicknameConfig,
+  getBojAuthTokenConfig,
+  joinConfig,
+} from './config';
+import { Provider } from 'src/types/user';
+import { IUser, IUserInputDTO } from 'src/interfaces/user/IUser';
+
+const login = async (
+  provider: Provider,
+  token: string
+): Promise<Pick<IUser, 'email' | 'nickname'>> =>
+  axios(loginConfig(provider, token)).then((res) => {
+    return res.data;
+  });
+
+const validateNickname = async (
+  nickname: string
+): Promise<{ isValidated: boolean }> =>
+  axios(validateNicknameConfig(nickname)).then((res) => {
+    return res.data;
+  });
+
+const getBojIdAuthToken = async (
+  bojId: string
+): Promise<{ authenticationToken: string }> =>
+  axios(getBojAuthTokenConfig(bojId)).then((res) => {
+    return res.data;
+  });
+
+const authenticateBojId = async (
+  bojId: string
+): Promise<{ authenticationToken: string }> =>
+  axios(authenticateBojIdConfig(bojId)).then((res) => {
+    return res.data;
+  });
+
+const join = async (
+  data: IUserInputDTO,
+  token: string
+): Promise<IUserInputDTO> =>
+  axios(joinConfig(data, token)).then((res) => {
+    return res.data;
+  });
+
+const AuthService = {
+  login,
+  validateNickname,
+  getBojIdAuthToken,
+  authenticateBojId,
+  join,
+};
+
+export default AuthService;
