2c670de1f30e261be16780849d96f63bf2dbd4e6
gywlsp <gywls517@gmail.com>
2021-07-21 20:51:22 +0900

    fix: :bug: fix getQueryString not working when there is no query parameter
    add default value {}

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,4 +1,4 @@
-export const getQueryString = (queryParameter: Record<string, any>) =>
+export const getQueryString = (queryParameter: Record<string, any> = {}) =>
   Object.entries(queryParameter).reduce((acc, curr, i) => {
     const [key, value] = curr;
     return acc.concat(`${i ? '&' : '?'}${key}=${value}`);
