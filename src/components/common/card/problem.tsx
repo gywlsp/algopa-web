b613fc0eeab61601c78c0424a02c1e07d96e7685
gywlsp <gywls517@gmail.com>
2021-07-21 20:56:36 +0900

    chore: :label: make ProblemCardProps with IProblem

--- src/components/common/card/problem.tsx
+++ src/components/common/card/problem.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import Link from '../link';
 import styled from 'styled-components';
 
-import { BLUE_GREEN, GREY, WHITE } from 'src/constants/colors';
+import Img from '../img';
 import P from '../p';
+import { BLUE_GREEN, GREY, WHITE } from 'src/constants/colors';
+
+import { IProblem } from 'src/interfaces/problem/IProblem';
 
 export type ProblemCardProps = {
   index?: number;
-  id?: number | string;
-  title?: string;
-  level?: string;
   isRouting?: boolean;
   className?: string;
-};
+} & Partial<IProblem>;
 
 export default function ProblemCard({
   index,
   className,
-  id = 1260,
-  level,
+  id,
+  number = 1260,
+  levelImgLink,
   isRouting = true,
   title = '문제 제목',
 }: ProblemCardProps) {
@@ -32,8 +33,8 @@ export default function ProblemCard({
             </P>
           </IndexLabel>
         )}
-        <Level>난이도</Level>
-        <StyledP>{id}</StyledP>
+        <Img width="100%" height="auto" src={levelImgLink} alt={title} cover />
+        <StyledP>{number}</StyledP>
         <StyledP>{title}</StyledP>
       </StyledLink>
     );
@@ -48,8 +49,8 @@ export default function ProblemCard({
           </P>
         </IndexLabel>
       )}
-      <Level>난이도</Level>
-      <P>{id}</P>
+      <Img width="100%" height="auto" src={levelImgLink} alt={title} cover />
+      <P>{number}</P>
       <P>{title}</P>
     </Wrapper>
   );
@@ -85,16 +86,6 @@ const IndexLabel = styled.div`
   width: 2.8rem;
   height: 2.8rem;
   background-color: ${BLUE_GREEN[500]};
-`;
-
-const Level = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 12rem;
-  margin-bottom: 2rem;
-  border: 1px solid ${GREY[400]};
 `;
 
 const StyledP = styled(P).attrs({ level: 4 })`
