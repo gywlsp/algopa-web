34804f0c8923f9ba9956253cb259b3dcacf22575
gywlsp <gywls517@gmail.com>
2021-07-21 15:40:00 +0900

    feat: :label: update type IUser, add type IUserReadDTO

--- src/interfaces/user/IUser.ts
+++ src/interfaces/user/IUser.ts
@@ -4,9 +4,16 @@ export interface IUser {
   provider: string;
   nickname: string;
   bojId?: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export type IUserInputDTO = Pick<
   IUser,
   'email' | 'provider' | 'nickname' | 'bojId'
+>;
+
+export type IUserReadDTO = Pick<
+  IUser,
+  'email' | 'nickname' | 'bojId' | 'accessToken' | 'refreshToken'
 >;
