77b7aade47ad8682d36bb51fc1eaecd9335e7328
gywlsp <gywls517@gmail.com>
2021-07-22 15:51:30 +0900

    style: change import order on problem hook file

--- src/hooks/api/problem.ts
+++ src/hooks/api/problem.ts
@@ -1,7 +1,8 @@
+import useRequest from '.';
+
 import { IProblemReadDTO } from 'src/interfaces/problem/IProblem';
 import { ProblemListRequestParams } from 'src/types/problem';
 import { listConfig } from 'src/services/api/problem/config';
-import useRequest from '.';
 
 export const useProblemList = (params?: ProblemListRequestParams) =>
   useRequest<IProblemReadDTO[]>(listConfig(params));
