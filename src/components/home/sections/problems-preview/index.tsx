26ed5368b082825e5aa2ece06d7264202a0a2155
gywlsp <gywls517@gmail.com>
2021-07-27 22:01:09 +0900

    feat: :sparkles: add ProblemCardSkeleton to ProblemsPreviewSection

--- src/components/home/sections/problems-preview/index.tsx
+++ src/components/home/sections/problems-preview/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Section from 'src/components/common/section';
-import Link from 'src/components/common/link';
 import ProblemCard from 'src/components/common/card/problem';
 import MoreButton from './more-button';
-import { GREY, WHITE } from 'src/constants/colors';
+import ProblemCardSkeleton from 'src/components/skeletons/card/problem';
 
 import { useProblemList } from 'src/hooks/api/problem';
 import { useMe } from 'src/hooks/api/user';
@@ -21,10 +20,12 @@ export default function ProblemsPreviewSection() {
   return (
     <Wrapper title={sectionTitle}>
       <CardsWrapper>
-        <MoreButton isLoggedIn={!!userData} />
+        {recommendedProblems && <MoreButton isLoggedIn={!!userData} />}
         {recommendedProblems?.reverse().map((problem, i) => (
           <ProblemCard key={i} {...problem} />
         ))}
+        {!recommendedProblems &&
+          [...Array(5)].map((_, i) => <ProblemCardSkeleton isLast={i === 0} />)}
       </CardsWrapper>
     </Wrapper>
   );
@@ -42,23 +43,4 @@ const CardsWrapper = styled.div`
   width: 100%;
   height: 27.2rem;
   overflow-y: hidden;
-`;
-
-const P = styled.p`
-  font-size: 1.8rem;
-  margin-bottom: 1.2rem;
-  color: ${WHITE};
-`;
-
-const MoreLink = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  min-width: 24rem;
-  height: 27.2rem;
-  padding: 2rem;
-  border: 1px solid ${GREY[400]};
-  background-color: rgba(0, 0, 0, 0.4);
 `;
