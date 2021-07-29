17c1d819ae9778712148675445e6db345391e2f7
gywlsp <gywls517@gmail.com>
2021-07-29 14:26:29 +0900

    chore: :fire: comment out unused roadmap data

--- src/data/roadmap.ts
+++ src/data/roadmap.ts
@@ -12,43 +12,70 @@ import {
 export const FAILURE_RATE_COLORS = [PASTEL_RED, PASTEL_YELLOW, PASTEL_BLUE];
 export const SOLVED_COLORS = [SUCCESS_BLUE, FAILURE_RED, GREY[500]];
 
+// const ALGORITHM_CATEGORIES = [
+//   '구현',
+//   '재귀',
+//   '완전탐색',
+//   '분할정복',
+//   '정렬',
+//   '이분탐색',
+//   '다이나믹프로그래밍',
+//   '그리디',
+//   '문자열',
+//   '배열, 큐, 스택, 덱',
+//   '해시',
+//   '트리',
+//   '세그먼트트리',
+//   '힙',
+//   '우선순위큐',
+//   '깊이우선탐색',
+//   '너비우선탐색',
+//   '백트래킹',
+//   '최단경로',
+//   '비트마스킹',
+//   '투포인터',
+//   '위상정렬',
+// ];
 
-const ALGORITHM_CATEGORIES = [
-  '구현',
-  '재귀',
-  '완전탐색',
-  '분할정복',
-  '정렬',
-  '이분탐색',
-  '다이나믹프로그래밍',
-  '그리디',
-  '문자열',
-  '배열, 큐, 스택, 덱',
-  '해시',
-  '트리',
-  '세그먼트트리',
-  '힙',
-  '우선순위큐',
-  '깊이우선탐색',
-  '너비우선탐색',
-  '백트래킹',
-  '최단경로',
-  '비트마스킹',
-  '투포인터',
-  '위상정렬',
-];
+// export const CATEGORY_NODES = ALGORITHM_CATEGORIES.map((category, i) => ({
+//   id: String(i),
+//   label: `[${i + 1}] ${category}`,
+//   color: FAILURE_RATE_COLORS[i % 3],
+//   shape: 'circle',
+// }));
 
-export const CATEGORY_NODES = ALGORITHM_CATEGORIES.map((category, i) => ({
-  id: String(i),
-  label: `[${i + 1}] ${category}`,
-  color: BLUE_GREEN[300],
-  shape: 'circle',
-}));
+// export const CATEGORY_EDGES = ALGORITHM_CATEGORIES.slice(1).map((_, i) => ({
+//   from: String(i),
+//   to: String(i + 1),
+// }));
 
-export const CATEGORY_EDGES = ALGORITHM_CATEGORIES.slice(1).map((_, i) => ({
-  from: String(i),
-  to: String(i + 1),
-}));
+// export const PROBLEM_NODES = CATEGORY_NODES.reduce((acc, { id: _id }) => {
+//   const problems = [];
+//   for (let i = 0; i < 3; i++) {
+//     const id = _id + '-' + i;
+//     const label = `문제 ${id}`;
+//     problems.push({
+//       id,
+//       label,
+//       color: BLUE_GREEN[100],
+//       shape: 'circle',
+//     });
+//   }
+//   return acc.concat(problems);
+// }, []);
+
+// export const PROBLEM_EDGES = PROBLEM_NODES.map(({ id }) => {
+//   const categoryId = id.slice(0, id.indexOf('-'));
+//   return { from: id, to: categoryId };
+// });
+
+// export const NODES = CATEGORY_NODES.concat(PROBLEM_NODES);
+// export const EDGES = CATEGORY_EDGES.concat(PROBLEM_EDGES);
+
+// export const GRAPH_DATA = {
+//   nodes: NODES,
+//   edges: EDGES,
+// };
 
 export const getCategoryNodeStyle = (isLoggedIn: boolean, index: number) => ({
   color: isLoggedIn
@@ -61,9 +88,9 @@ export const getCategoryNodeStyle = (isLoggedIn: boolean, index: number) => ({
         },
       }
     : BLUE_GREEN[200],
-      shape: 'circle',
+  shape: 'circle',
   font: { size: 16 },
-    });
+});
 
 export const getProblemNodeStyle = (isLoggedIn: boolean, index: number) => ({
   color: isLoggedIn
@@ -74,7 +101,7 @@ export const getProblemNodeStyle = (isLoggedIn: boolean, index: number) => ({
           background: BLUE_GREEN[100],
           border: SOLVED_COLORS[index % 3],
         },
-  }
+      }
     : BLUE_GREEN[100],
   shape: 'circle',
 });
