707a5ef37e49c0c57ed7bd78a0655f8905814fe2
gywlsp <gywls517@gmail.com>
2021-07-29 13:52:13 +0900

    chore: :label: make RoadmapNode, RoadmapEdge type with vis-network type

--- src/types/roadmap.ts
+++ src/types/roadmap.ts
@@ -1,3 +1,4 @@
+import { Edge, Node } from 'vis-network';
 import { ICategoryReadDTO } from 'src/interfaces/category/ICategory';
 import { IProblemReadDTO } from 'src/interfaces/problem/IProblem';
 
@@ -9,16 +10,10 @@ export type RoadmapDTO = {
 
 export type RoadmapNode = {
   nodeId: string;
-  id: string;
-  label: string;
-  color: string;
-  shape: string;
-};
+} & Node;
 
 export type RoadmapCategoryNode = ICategoryReadDTO & RoadmapNode;
 export type RoadmapProblemNode = IProblemReadDTO & RoadmapNode;
 
-export type RoadmapEdge = {
-  from: string;
-  to: string;
-};
+export type RoadmapEdge = Edge;
+
