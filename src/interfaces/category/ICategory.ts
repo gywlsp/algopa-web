131cc177f52d446d43458c1a75bacd1f631915b8
gywlsp <gywls517@gmail.com>
2021-08-02 12:53:12 +0900

    feat: :label: add order property to ICategory

--- src/interfaces/category/ICategory.ts
+++ src/interfaces/category/ICategory.ts
@@ -3,6 +3,7 @@ import { UserCategoryInfo } from 'src/types/user';
 export interface ICategory {
   id: number;
   name: string;
+  order: number;
 }
 
 export type ICategoryReadDTO = ICategory & UserCategoryInfo;
