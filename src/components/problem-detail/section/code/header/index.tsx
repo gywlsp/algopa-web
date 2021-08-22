1d533e28fda795d4579dbf29123272708533ae83
gywlsp <gywls517@gmail.com>
2021-08-22 22:41:18 +0900

    chore: :lipstick: add ProblemDetailCodeSectionHeader UI
    only title

---
+++ src/components/problem-detail/section/code/header/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import P from 'src/components/common/p';
+import { GREY } from 'src/constants/colors';
+
+export default function ProblemDetailCodeSectionHeader() {
+  return (
+    <Wrapper>
+      <P level={2} color={GREY[400]}>
+        코드
+      </P>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 4.4rem;
+  padding: 1.2rem;
+  margin-bottom: 0.8rem;
+  border-bottom: 0.1rem solid ${GREY[900]};
+`;
