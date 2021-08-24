862dd27bfbdfdd7a6182c0ab7093e022c710de49
gywlsp <gywls517@gmail.com>
2021-08-24 23:19:11 +0900

    feat: :sparkles: add useProblem

--- src/hooks/api/problem.ts
+++ src/hooks/api/problem.ts
@@ -2,7 +2,13 @@ import useRequest from '.';
 
 import { IProblemReadDTO } from 'src/interfaces/problem/IProblem';
 import { ProblemListRequestParams } from 'src/types/problem';
-import { recommendationListConfig } from 'src/services/api/problem/config';
+import {
+  readConfig,
+  recommendationListConfig,
+} from 'src/services/api/problem/config';
 
 export const useRecommendedProblemList = (params?: ProblemListRequestParams) =>
   useRequest<IProblemReadDTO[]>(recommendationListConfig(params));
+
+export const useProblem = (id: number) =>
+  useRequest<IProblemReadDTO>(readConfig(id));
