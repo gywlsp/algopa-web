db7d5862bc76f27ebed20715e665560a505aaa93
gywlsp <gywls517@gmail.com>
2021-09-02 23:16:53 +0900

    feat: :sparkles: add useCodeList

---
+++ src/hooks/api/code.ts
@@ -0,0 +1,7 @@
+import useRequest from '.';
+
+import { ICode } from 'src/interfaces/code/ICode';
+import { listConfig } from 'src/services/api/code/config';
+
+export const useCodeList = (problemId?: number) =>
+  useRequest<ICode[]>(listConfig(problemId));
