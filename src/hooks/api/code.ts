ba34df89d7ec659af6c6f3329450712f8d1c5c2d
gywlsp <gywls517@gmail.com>
2021-09-14 20:26:14 +0900

    chore: :wrench: disable useCodeList revalidation

--- src/hooks/api/code.ts
+++ src/hooks/api/code.ts
@@ -4,4 +4,8 @@ import { ICode } from 'src/interfaces/code/ICode';
 import { listConfig } from 'src/services/api/code/config';
 
 export const useCodeList = (problemId?: number) =>
-  useRequest<ICode[]>(listConfig(problemId));
+  useRequest<ICode[]>(listConfig(problemId), {
+    revalidateOnFocus: false,
+    revalidateOnMount: false,
+    revalidateOnReconnect: false,
+  });
