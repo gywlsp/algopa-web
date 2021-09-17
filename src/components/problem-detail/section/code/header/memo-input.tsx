3d61bfb829bab6492d717a2d2ea88ddf42f49c66
gywlsp <gywls517@gmail.com>
2021-09-17 21:01:03 +0900

    feat: :sparkles: add create event index feature to MemoTitleInput

--- src/components/problem-detail/section/code/header/memo-input.tsx
+++ src/components/problem-detail/section/code/header/memo-input.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import CheckIcon from 'src/assets/icons/check';
 import { GREY } from 'src/constants/colors';
 
-export type TextInputProps = Pick<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  'onChange'
-> & {
-  value?: string;
-};
+import { ICodeReadDTO } from 'src/interfaces/code/ICode';
+import CodeService from 'src/services/api/code';
 
-export default function MemoInput({ value, onChange }: TextInputProps) {
+export type MemoTitleInputProps = Pick<ICodeReadDTO, 'lastEventId'>;
+
+export default function MemoTitleInput({ lastEventId }: MemoTitleInputProps) {
+  const [memoTitle, setMemoTitle] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMemoTitle(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    if (!lastEventId) {
+      alert('코드 변경 내역이 없습니다.');
+      return;
+    }
+    try {
+      await CodeService.createEventIndex(lastEventId, {
+        title: memoTitle,
+        content: '',
+      });
+      alert('인덱스가 생성되었습니다.');
+    } catch (err) {
+      alert('인덱스 생성에 실패하였습니다.');
+    }
+  };
+
   return (
-    <InputWrapper>
-      <StyledInput value={value} onChange={onChange} placeholder="Message" />
-    </InputWrapper>
+    <>
+      <InputWrapper>
+        <StyledInput
+          value={memoTitle}
+          onChange={handleChange}
+          placeholder="Message (optional)"
+        />
+      </InputWrapper>
+      <Button onClick={handleSubmit}>
+        <CheckIcon style={{ width: '1.6rem', height: '1.6rem' }} />
+      </Button>
+    </>
   );
 }
 
@@ -48,4 +78,19 @@ const StyledInput = styled.input`
     color: ${GREY[500]};
     font-weight: 400;
   }
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${GREY[750]};
+  color: ${GREY[400]};
+  width: 2.4rem;
+  height: 2.4rem;
+  margin-left: 0.6rem;
+  padding: 0;
+  border: none;
+  border-radius: 0.2rem;
+  cursor: pointer;
 `;
