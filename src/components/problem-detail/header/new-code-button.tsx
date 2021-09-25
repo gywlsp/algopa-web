69a8e73a9739fe8fedcac4423cdaf603e70d2b3d
gywlsp <gywls517@gmail.com>
2021-09-25 23:54:39 +0900

    refactor: :recycle: refactor component NewCodeButton with recoil atom problem

--- src/components/problem-detail/header/new-code-button.tsx
+++ src/components/problem-detail/header/new-code-button.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
 
 import Button from 'src/components/common/button';
 
-import { IProblemReadDTO } from 'src/interfaces/problem/IProblem';
 import CodeService from 'src/services/api/code';
+import { problem } from 'src/modules/atoms/problem';
 
-export type NewCodeButtonProps = Pick<IProblemReadDTO, 'id'>;
+export default function NewCodeButton() {
+  const problemData = useRecoilValue(problem);
 
-export default function NewCodeButton({ id }: NewCodeButtonProps) {
   const createNewCode = async () => {
     if (!confirm('문제를 새로 푸시겠습니까?')) {
       return;
     }
     try {
-      await CodeService.create(id);
+      await CodeService.create(problemData?.id);
     } catch (err) {
       alert('코드 생성 실패');
     }
