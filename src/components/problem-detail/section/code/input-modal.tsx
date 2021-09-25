5408ca22a504d9d26a0214f3211aa9de3ba171fd
gywlsp <gywls517@gmail.com>
2021-09-25 23:58:55 +0900

    refactor: :recycle: refactor component CodeRunInputModal with useCodeRun

--- src/components/problem-detail/section/code/input-modal.tsx
+++ src/components/problem-detail/section/code/input-modal.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import Modal from 'src/components/common/modal';
 import { GREY } from 'src/constants/colors';
 
-import CodeService from 'src/services/api/code';
 import { ICode } from 'src/interfaces/code/ICode';
 import { RunOutput } from 'src/types/code';
+import { useCodeRun } from 'src/hooks/api/code';
 
 export type CodeRunInputModalProps = Pick<ICode, 'id' | 'text'> & {
   isOpen: boolean;
@@ -14,42 +14,22 @@ export type CodeRunInputModalProps = Pick<ICode, 'id' | 'text'> & {
   setRunOutput: React.Dispatch<React.SetStateAction<RunOutput>>;
 };
 
-export default function CodeRunInputModal({
-  id,
-  text,
-  isOpen,
-  onClose,
-  setRunOutput,
-}: CodeRunInputModalProps) {
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value);
-  };
-
-  const handleSubmit = async () => {
-    try {
-      const data = await CodeService.execute(id, { text, input });
-      setRunOutput(data);
-      setInput('');
-      onClose();
-    } catch (err) {
-      setRunOutput({ success: false, result: '코드 실행 실패' });
-    }
-  };
+export default function CodeRunInputModal() {
+  const { input, onInputChange, isModalOpen, closeModal, onCodeSubmit } =
+    useCodeRun();
 
   return (
     <Modal
       title="입력"
-      isOpen={isOpen}
-      onClose={onClose}
+      isOpen={isModalOpen}
+      onClose={closeModal}
       okText="코드 실행"
-      onOk={handleSubmit}
+      onOk={onCodeSubmit}
       theme="dark"
     >
       <Textarea
         value={input}
-        onChange={handleInputChange}
+        onChange={onInputChange}
         placeholder="input 값을 입력해주세요.(optional)"
       />
     </Modal>
