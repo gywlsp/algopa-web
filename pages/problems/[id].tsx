faede1f5c66237858c11465224f286efbc9ad27b
gywlsp <gywls517@gmail.com>
2021-09-26 00:00:56 +0900

    chore: :fire: remove codes refactored with hooks from ProblemDetailPage

--- pages/problems/[id].tsx
+++ pages/problems/[id].tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
-
 import ProblemDetailHeader from 'src/components/problem-detail/header';
 import ProblemContentSection from 'src/components/problem-detail/section/problem-content';
 import ProblemCodeSection from 'src/components/problem-detail/section/code';
 import ProblemContentSkeleton from 'src/components/skeletons/problem-content';
 import { GREY } from 'src/constants/colors';
 
-import { useCodeList } from 'src/hooks/api/code';
 import { useRecentAuthTokens } from 'src/hooks/api/auth';
 import { useProblem } from 'src/hooks/api/problem';
 
@@ -16,33 +14,14 @@ export default function ProblemDetailPage() {
   useRecentAuthTokens(true);
   const router = useRouter();
   const { data } = useProblem(+router.query.id);
-  const { data: codes } = useCodeList(data?.id);
-  const [selectedCodeId, setSelectedCodeId] = useState(undefined);
-
-  useEffect(() => {
-    if (codes && codes[0].id !== selectedCodeId) {
-      setSelectedCodeId(codes[0].id);
-    }
-  }, [codes]);
-
-  const selectedCode = codes?.find((code) => code.id === selectedCodeId);
-  const selectCode = (id: string) => {
-    setSelectedCodeId(id);
-  };
 
   return (
     <Wrapper>
-      <ProblemDetailHeader
-        id={data?.id}
-        title={data?.title}
-        codes={codes}
-        selectedCodeId={selectedCodeId}
-        selectCode={selectCode}
-      />
+      <ProblemDetailHeader />
       <ContentWrapper>
-        {data && <ProblemContentSection contentHTML={data?.contentHTML} />}
+        {data && <ProblemContentSection />}
         {!data && <ProblemContentSkeleton />}
-        <ProblemCodeSection code={selectedCode} />
+        <ProblemCodeSection />
       </ContentWrapper>
     </Wrapper>
   );
