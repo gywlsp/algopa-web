397f8de5b226679dc08af4d93ede881d8fec2bf3
gywlsp <gywls517@gmail.com>
2021-09-27 20:29:18 +0900

    feat: :sparkles: add NoteViewButton

---
+++ src/components/problem-detail/section/history/header/note-view-button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+import { BLUE_GREEN, WHITE } from 'src/constants/colors';
+
+import { problemPageRightSectionType } from 'src/modules/atoms/problem';
+
+export default function NoteViewButton() {
+  const setRightSectionType = useSetRecoilState(problemPageRightSectionType);
+
+  const handleButtonClick = async () => {
+    setRightSectionType('note');
+  };
+
+  return <Button onClick={handleButtonClick}>풀이 노트</Button>;
+}
+const Button = styled.button`
+  font-size: 1.2rem;
+  background-color: ${BLUE_GREEN[600]};
+  color: ${WHITE};
+  padding: 0.6rem 0.8rem;
+  margin-left: 1.2rem;
+  border: none;
+  border-radius: 0.2rem;
+  cursor: pointer;
+`;
