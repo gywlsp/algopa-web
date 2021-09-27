3dc9e707bd35c3a1d488c27d5c65be1e81e75a74
gywlsp <gywls517@gmail.com>
2021-09-27 20:28:48 +0900

    feat: :sparkles: add atom selector selectedCodeEvent

--- src/modules/selectors/code.ts
+++ src/modules/selectors/code.ts
@@ -1,5 +1,10 @@
 import { selector } from 'recoil';
-import { problemCodes, selectedProblemCodeId } from '../atoms/code';
+import {
+  codeEvents,
+  problemCodes,
+  selectedCodeEventId,
+  selectedProblemCodeId,
+} from '../atoms/code';
 
 export const selectedProblemCode = selector({
   key: 'selectedProblemCode',
@@ -7,5 +12,14 @@ export const selectedProblemCode = selector({
     const codes = get(problemCodes);
     const selectedCodeId = get(selectedProblemCodeId);
     return codes?.find((code) => code.id === selectedCodeId);
+  },
+});
+
+export const selectedCodeEvent = selector({
+  key: 'selectedCodeEvent',
+  get: ({ get }) => {
+    const events = get(codeEvents);
+    const selectedEventId = get(selectedCodeEventId);
+    return events?.find((event) => event.id === selectedEventId);
   },
 });
