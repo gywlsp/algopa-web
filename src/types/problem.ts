741ad456174760177ba9dbca9da1bf0dfcfcdf10
gywlsp <gywls517@gmail.com>
2021-09-27 19:03:57 +0900

    chore: :label: add type ProblemPageRightSectionType

--- src/types/problem.ts
+++ src/types/problem.ts
@@ -3,3 +3,5 @@ import { ListRequestParams } from '.';
 export type ProblemListRequestParams = {
   type?: 'next' | 'less' | 'wrong';
 } & ListRequestParams;
+
+export type ProblemPageRightSectionType = 'code' | 'history' | 'note';
