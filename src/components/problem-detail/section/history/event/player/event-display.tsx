d4a7a7b459d79963d19e784af31e8708b935a268
gywlsp <gywls517@gmail.com>
2021-09-29 01:09:30 +0900

    feat: :sparkles: add CodeHistoryPlayerEventDisplay

---
+++ src/components/problem-detail/section/history/event/player/event-display.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+
+import P from 'src/components/common/p';
+import { GREY } from 'src/constants/colors';
+
+import { codeEvents } from 'src/modules/atoms/code';
+import { selectedCodeEvent } from 'src/modules/selectors/code';
+
+export default function CodeHistoryPlayerEventDisplay() {
+  const events = useRecoilValue(codeEvents);
+  const selectedEvent = useRecoilValue(selectedCodeEvent);
+
+  return (
+    <Wrapper>
+      <StyledP>
+        <Strong>{selectedEvent.order}</Strong> / {events.length}
+      </StyledP>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 10rem;
+`;
+
+const Strong = styled.strong`
+  color: ${GREY[400]};
+  font-weight: 400;
+`;
+
+const StyledP = styled(P).attrs({
+  level: 2,
+  color: GREY[450],
+})`
+  margin-right: 0.2rem;
+`;
