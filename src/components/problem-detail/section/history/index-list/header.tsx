d50cbcb5fddcb6665344871c083f7124bf7d8d6e
gywlsp <gywls517@gmail.com>
2021-09-29 01:02:30 +0900

    chore: :lipstick: change IndexListSectionHeader height, title style

--- src/components/problem-detail/section/history/index-list/header.tsx
+++ src/components/problem-detail/section/history/index-list/header.tsx
@@ -16,11 +16,11 @@ const Wrapper = styled.header`
   display: flex;
   align-items: center;
   width: 100%;
-  height: 3.2rem;
+  height: 4.4rem;
   padding: 1.2rem;
   border-bottom: 0.1rem solid ${GREY[900]};
 `;
 
-const Title = styled(P).attrs({ level: 1, color: GREY[400], fontWeight: 500 })`
+const Title = styled(P).attrs({ level: 2, color: GREY[400] })`
   margin-right: auto;
 `;
