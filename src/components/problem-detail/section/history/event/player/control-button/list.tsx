a4c2ea3d0eeaeff797249291f12a4f561810c2d8
gywlsp <gywls517@gmail.com>
2021-09-29 21:57:34 +0900

    chore: :fire: remove unused styled component on PlayerControlButtonList

--- src/components/problem-detail/section/history/event/player/control-button/list.tsx
+++ src/components/problem-detail/section/history/event/player/control-button/list.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SkipButton from './skip';
-import { GREY } from 'src/constants/colors';
 
 export default function CodeHistoryPlayerControlButtonList() {
   return (
@@ -19,19 +18,4 @@ const Wrapper = styled.div`
   width: fit-content;
   height: 100%;
   margin: 0 auto;
-`;
-
-const Button = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 2.4rem;
-  height: 2.4rem;
-  font-size: 1.4rem;
-  padding: 0;
-  margin: 0 0.4rem;
-  color: ${GREY[400]};
-  background-color: ${GREY[800]};
-  border: 0.1rem solid ${GREY[400]};
-  border-radius: 999px;
 `;
