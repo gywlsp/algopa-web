bed6ea980821fe4eb6db705751c80865b6331779
gywlsp <gywls517@gmail.com>
2021-09-30 14:40:33 +0900

    chore: :lipstick: change CodeHistoryPlayerSkipButton style

--- src/components/problem-detail/section/history/event/player/control-button/skip.tsx
+++ src/components/problem-detail/section/history/event/player/control-button/skip.tsx
@@ -34,18 +34,22 @@ export default function CodeHistoryPlayerSkipButton({
   return <Button onClick={handleButtonClick}>{buttonText}</Button>;
 }
 
-const Button = styled.div`
+const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 2.4rem;
+  width: 2.8rem;
   height: 2.4rem;
-  font-size: 1.2rem;
+  font-size: 0.8rem;
   padding: 0;
   margin: 0 0.4rem;
   color: ${GREY[400]};
   background-color: ${GREY[800]};
-  border: 0.1rem solid ${GREY[400]};
+  &:hover {
+    background-color: ${GREY[700]};
+    transition: all 0.2s;
+  }
+  border: 0.1rem solid ${GREY[500]};
   border-radius: 999px;
   cursor: pointer;
 `;
