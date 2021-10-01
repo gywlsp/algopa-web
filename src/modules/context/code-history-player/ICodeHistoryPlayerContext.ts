5d9c3da33a9a8e39778b9e86d67704383818ec2f
gywlsp <gywls517@gmail.com>
2021-10-01 23:46:24 +0900

    feat: :label: add ICodeHistoryPlayerContext

---
+++ src/modules/context/code-history-player/ICodeHistoryPlayerContext.ts
@@ -0,0 +1,21 @@
+import { MutableRefObject } from 'react';
+import { CodeTextChangeEvent } from 'src/types/code';
+
+export interface ICodeHistoryPlayerContext {
+  state: {
+    events: CodeTextChangeEvent[];
+    selectedEventOrder: number;
+    timelineRef: MutableRefObject<any>;
+    scrubberRef: MutableRefObject<any>;
+    isPlaying: boolean;
+    playRate: string;
+    playSpeed: number;
+    playSec: number;
+  };
+  action: {
+    updatePlaySpeed: (speed: number) => void;
+    skipEvent: (value: number) => void;
+    togglePlaying: () => void;
+    initPlaying: () => void;
+  };
+}
