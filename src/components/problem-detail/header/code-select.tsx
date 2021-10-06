02e5fdebb009244b210756dea8f70a0117a427e5
gywlsp <gywls517@gmail.com>
2021-10-06 10:23:08 +0900

    feat: :sparkles: check event exists when user try to select another code and right section type is history

--- src/components/problem-detail/header/code-select.tsx
+++ src/components/problem-detail/header/code-select.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useResetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import P from 'src/components/common/p';
 import Select from 'src/components/common/select';
@@ -8,13 +8,12 @@ import { BLUE_GREEN } from 'src/constants/colors';
 
 import { problemPageRightSectionType } from 'src/modules/atoms/problem';
 import { useProblemCodes, useSelectedCode } from 'src/hooks/api/code';
+import CodeService from 'src/services/api/code';
 
 export default function CodeSelect() {
   const { data: codes } = useProblemCodes();
   const { id: selectedCodeId, select } = useSelectedCode();
-  const resetRightSectionType = useResetRecoilState(
-    problemPageRightSectionType
-  );
+  const rightSectionType = useRecoilValue(problemPageRightSectionType);
   const selectOptions = codes?.map(({ id, tryCount }) => ({
     key: id,
     label: `try_${tryCount}`,
@@ -22,11 +21,23 @@ export default function CodeSelect() {
     selected: id === selectedCodeId,
   }));
 
-  const handleSelectedCodeChange = (
+  const handleSelectedCodeChange = async (
     e: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    resetRightSectionType();
-    select(e.target.value);
+    const { value } = e.target;
+    if (rightSectionType === 'history') {
+      try {
+        const events = await CodeService.eventList(value);
+        if (!events?.length) {
+          alert('해당 코드의 풀이 내역이 없습니다.');
+          return;
+        }
+      } catch (err) {
+        alert('해당 코드의 풀이 내역을 불러올 수 없습니다.');
+        console.log(err);
+      }
+    }
+    select(value);
   };
 
   return (
