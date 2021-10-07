92427d78862804bad4fe7b2b61102b863f8d9564
gywlsp <gywls517@gmail.com>
2021-10-08 00:39:52 +0900

    refactor: :recycle: use DRAFT_STYLE_BUTTONS data on NoteEditorStyleToggleButtonList

--- src/components/problem-detail/section/note/content/editor/styling-button/list.tsx
+++ src/components/problem-detail/section/note/content/editor/styling-button/list.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import StylingButton from '.';
+import StyleToggleButton from '.';
 
-const BUTTON_VALUES = [
-  { title: 'H1', value: 'header-one' },
-  { title: 'H2', value: 'header-two' },
-  { title: 'H3', value: 'header-three' },
-  { title: 'BLOCKQUOTE', value: 'blockquote' },
-  { title: 'UL', value: 'unordered-list-item' },
-  { title: 'OL', value: 'ordered-list-item' },
-  { title: 'BOLD', value: 'bold' },
-  { title: 'UNDERLINE', value: 'underline' },
-  { title: 'CODEBLOCK', value: 'code-block' },
-  { title: '</>', value: 'code' },
-];
+import { DRAFT_STYLE_BUTTONS } from 'src/data/note';
 
-export default function NoteEditorStylingButtonList() {
+export default function NoteEditorStyleToggleButtonList() {
   return (
     <Wrapper>
-      {BUTTON_VALUES.map(({ title, value }) => (
-        <StylingButton
-          key={value}
-          title={title}
-          onClick={() => {
-            console.log(value);
-          }}
-        />
+      {DRAFT_STYLE_BUTTONS.map(({ title, value }) => (
+        <StyleToggleButton key={value} title={title} value={value} />
       ))}
     </Wrapper>
   );
