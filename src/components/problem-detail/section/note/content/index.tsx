d344d8ec6d986f4a4ff59c3d96c8699d1fe0e287
gywlsp <gywls517@gmail.com>
2021-10-08 22:00:05 +0900

    feat: :sparkles: add no existing note GuideText to CodeNoteContentSection

--- src/components/problem-detail/section/note/content/index.tsx
+++ src/components/problem-detail/section/note/content/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import { useCodeNoteContext } from 'src/modules/context/code-note';
 import styled from 'styled-components';
 
 import CodeNoteEditor from './editor';
-import CodeNote from './view';
+import P from 'src/components/common/p';
+import { GREY } from 'src/constants/colors';
 
-export type CodeNoteContentSectionProps = {
-  isEditing: boolean;
-};
+export default function CodeNoteContentSection() {
+  const {
+    state: { note, isEditing },
+  } = useCodeNoteContext();
 
-export default function CodeNoteContentSection({
-  isEditing,
-}: CodeNoteContentSectionProps) {
   return (
     <Wrapper>
-      {isEditing && <CodeNoteEditor />}
-      {!isEditing && <CodeNote />}
+      {!isEditing && !note?.submitted && (
+        <GuideText>생성된 풀이노트가 없습니다.</GuideText>
+      )}
+      <CodeNoteEditor />
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   flex: 1;
   height: 100%;
+`;
+
+const GuideText = styled(P).attrs({ level: 1, color: GREY[500] })`
+  width: fit-content;
+  height: fit-content;
+  margin-bottom: 12rem;
 `;
