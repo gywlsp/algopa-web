6b53f19fea5742967b6162fef620bec2b2958efe
gywlsp <gywls517@gmail.com>
2021-10-19 00:45:07 +0900

    feat: :sparkles: add theme to common ProblemCard

--- src/components/common/card/problem.tsx
+++ src/components/common/card/problem.tsx
@@ -7,17 +7,26 @@ import P from '../p';
 import { BLUE_GREEN, GREY, WHITE } from 'src/constants/colors';
 
 import { IProblem } from 'src/interfaces/problem/IProblem';
+import { Theme } from 'src/types';
 
 export type ProblemCardProps = {
   index?: number;
   isRouting?: boolean;
   className?: string;
   problemLevel?: number;
+  theme?: Theme;
   //vis-network node에 level property 있어서 겹치지 않게 하기 위함
 } & Partial<IProblem>;
 
 function ProblemCard(props: ProblemCardProps) {
-  const { index, id, level, problemLevel, title = '문제 제목' } = props;
+  const {
+    index,
+    id,
+    level,
+    problemLevel,
+    title = '문제 제목',
+    theme = 'light',
+  } = props;
 
   const levelImgSrc = `https://static.solved.ac/tier_small/${
     problemLevel || level
@@ -32,11 +41,11 @@ function ProblemCard(props: ProblemCardProps) {
           </P>
         </IndexLabel>
       )}
-      <ImgWrapper>
+      <ImgWrapper theme={theme}>
         <Img src={levelImgSrc} alt={title} height="8rem" />
       </ImgWrapper>
-      <StyledP>{id}</StyledP>
-      <StyledP>{title}</StyledP>
+      <StyledP theme={theme}>{id}</StyledP>
+      <StyledP theme={theme}>{title}</StyledP>
     </ContentWrapper>
   );
 }
@@ -46,10 +55,10 @@ export default React.memo(ProblemCard);
 const ContentWrapper = (
   props: ProblemCardProps & { children: ReactNode | ReactNodeArray }
 ) => {
-  const { isRouting = true, className, id, children } = props;
+  const { isRouting = true, className, id, children, theme = 'light' } = props;
   if (isRouting) {
     return (
-      <StyledLink href={`/problems/${id}`} className={className}>
+      <StyledLink theme={theme} href={`/problems/${id}`} className={className}>
         {children}
       </StyledLink>
     );
@@ -57,7 +66,7 @@ const ContentWrapper = (
   return <Wrapper className={className}>{children}</Wrapper>;
 };
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link)<{ theme: Theme }>`
   position: relative;
   display: flex;
   flex-direction: column;
@@ -66,20 +75,7 @@ const StyledLink = styled(Link)`
   height: 27.2rem;
   margin-right: 1.2rem;
   padding: 2rem 2rem 0.8rem;
-  border: 1px solid ${GREY[400]};
-`;
-
-const A = styled.a`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  min-width: 22.4rem;
-  height: 27.2rem;
-  text-decoration: none;
-  margin-right: 1.2rem;
-  padding: 2rem 2rem 0.8rem;
-  border: 1px solid ${GREY[400]};
+  border: 1px solid ${({ theme }) => GREY[theme === 'light' ? 400 : 850]};
 `;
 
 const Wrapper = styled.div`
@@ -92,14 +88,14 @@ const Wrapper = styled.div`
   border: 1px solid ${GREY[400]};
 `;
 
-const ImgWrapper = styled.div`
+const ImgWrapper = styled.div<{ theme: Theme }>`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 14rem;
-  background-color: ${GREY[100]};
-  border: 1px solid ${GREY[400]};
+  background-color: ${({ theme }) => GREY[theme === 'light' ? 100 : 500]};
+  border: 1px solid ${({ theme }) => GREY[theme === 'light' ? 400 : 500]};
   margin-bottom: 2rem;
 `;
 
@@ -115,6 +111,11 @@ const IndexLabel = styled.div`
   background-color: ${BLUE_GREEN[500]};
 `;
 
-const StyledP = styled(P).attrs({ level: 4, width: '100%', ellipsis: true })`
+const StyledP = styled(P).attrs({
+  level: 4,
+  width: '100%',
+  ellipsis: true,
+})<{ theme: Theme }>`
   margin-bottom: 1.2rem;
+  color: ${({ theme }) => GREY[theme === 'light' ? 800 : 400]};
 `;
