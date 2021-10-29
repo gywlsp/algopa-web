5540fe66501a2d0d5d3e25218ac44b3664e860fc
gywlsp <gywls517@gmail.com>
2021-10-29 15:51:36 +0900

    feat: :sparkles: set ProblemCategorySwitch as rightComponent of ProblemDetailContentSection

--- src/components/problem-detail/section/problem-content.tsx
+++ src/components/problem-detail/section/problem-content.tsx
@@ -2,23 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 
+import ProblemCategorySwitch from 'src/components/common/category/switch';
+import ProblemCategoryTags from 'src/components/common/category/tags';
 import { GREY } from 'src/constants/colors';
 
-import { problem } from 'src/modules/atoms/problem';
+import { isProblemCategoryShown, problem } from 'src/modules/atoms/problem';
 
 export default function ProblemDetailContentSection() {
+  const isCategoryShown = useRecoilValue(isProblemCategoryShown);
   const problemData = useRecoilValue(problem);
 
   return (
-    <ProblemContent
-      className="problem-content"
-      dangerouslySetInnerHTML={{ __html: problemData?.contentHTML }}
-    />
+    <Wrapper>
+      <section
+        className="problem-content"
+        dangerouslySetInnerHTML={{ __html: problemData?.contentHTML }}
+      />
+      <CategorySwitch />
+      {isCategoryShown && (
+        <ProblemCategoryTags categories={problemData?.categories} />
+      )}
+    </Wrapper>
   );
 }
 
-const ProblemContent = styled.section`
+const Wrapper = styled.div`
   flex: 1;
   height: 100%;
+  background-color: #222;
+  overflow-y: scroll;
+  -ms-overflow-style: auto;
+  padding: 1.2rem;
   border-right: 0.1rem solid ${GREY[900]};
+  &::-webkit-scrollbar {
+    display: flex;
+    width: 0.4rem;
+    background: none;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${GREY[800]};
+  }
+`;
+
+const CategorySwitch = styled(ProblemCategorySwitch)`
+  margin-bottom: 0.8rem;
 `;
