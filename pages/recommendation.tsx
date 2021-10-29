ff655e91562560cd463ffbe2a82308ad3e94c722
gywlsp <gywls517@gmail.com>
2021-10-29 15:50:17 +0900

    feat: :sparkles: set ProblemCategorySwitch as rightComponent of ProblemRecommendationPage Section

--- pages/recommendation.tsx
+++ pages/recommendation.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import GlobalLayout from 'src/layouts/global';
 import Section from 'src/components/common/section';
 import RecommendedProblemsSection from 'src/components/recommendation/problems-section';
+import ProblemCategorySwitch from 'src/components/common/category/switch';
 
 import { useRecentAuthTokens } from 'src/hooks/api/auth';
 import { useMe } from 'src/hooks/api/user';
 
-export default function RecommendationPage() {
+export default function ProblemRecommendationPage() {
   useRecentAuthTokens(true);
   const { data: userData, error } = useMe();
 
@@ -17,7 +18,10 @@ export default function RecommendationPage() {
 
   return (
     <GlobalLayout>
-      <Section title={`${userData?.nickname || '회원'}님을 위한 추천 문제 🔍`}>
+      <Section
+        title={`${userData?.nickname || '회원'}님을 위한 추천 문제 🔍`}
+        rightComponent={<ProblemCategorySwitch />}
+      >
         <RecommendedProblemsSection type="next" />
         <RecommendedProblemsSection type="wrong" />
         <RecommendedProblemsSection type="less" />
